68299325fa348be93515c16344b9bb74b7791d51
BardakR <rom2_q@inbox.ru>
2016-02-06 02:27:34 +0300

    Урок 1 Заготовка для программы Змейка

---
+++ Shake/Shake/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Создано в SharpDevelop.
+ * Пользователь: РОМА
+ * Дата: 06.02.2016
+ * Время: 2:05
+ * 
+ * Для изменения этого шаблона используйте меню "Инструменты | Параметры | Кодирование | Стандартные заголовки".
+ */
+using System;
+
+namespace Shake
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World! Привет мир!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadLine();
+		}
+	}
+}
